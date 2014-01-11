0b6ebbb215f0b77a353afbfb4a822ad24b94a87f
Javier Garcia <javitid@gmail.com>
2014-01-11 10:29:12 +0100

    Añadido RestClient class

--- src/com/komponentes/oscomanager/ListaPedidos.java
+++ src/com/komponentes/oscomanager/ListaPedidos.java
@@ -1,16 +1,28 @@
 package com.komponentes.oscomanager;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class ListaPedidos extends Activity implements OnClickListener{
 
+	SharedPreferences preferences;
+	String URL, USER, PWD;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lista_pedidos);
+		
+		// Preferencias
+		preferences = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
+        URL = preferences.getString("servidor", "");
+        USER = preferences.getString("user", "");
+        PWD = preferences.getString("password", "");
 		
 		// Set up click listeners for all the buttons
 		View button1 = findViewById(R.id.button1);
@@ -33,6 +45,19 @@ public class ListaPedidos extends Activity implements OnClickListener{
 	
 	
     public void getPedidos(){
-    	// TODO: petición REST
+    	RestClient client = new RestClient(URL);
+	    //client.AddParam("param 1 name", "param 1 value");
+	    //client.AddHeader("header 1 name", "header 1 value");
+	     
+	    try {
+	        client.Execute(RestClient.RequestMethod.GET);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    String response = client.getResponse();
+	    if (response != null){
+	    	Log.i("REST service response: ", response);
+	    }
     }
 }
