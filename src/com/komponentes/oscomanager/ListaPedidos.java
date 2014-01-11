be7b5d596359225c1234fc58c2a7a4287f15251d
Javier Garcia <javitid@gmail.com>
2014-01-11 11:58:05 +0100

    - Añadido tratamiento para el elemento JSON que se espera recibir. - Añadida plantilla para pintar la lista - Añadido layout para cada elemento de la lista

--- src/com/komponentes/oscomanager/ListaPedidos.java
+++ src/com/komponentes/oscomanager/ListaPedidos.java
@@ -1,5 +1,13 @@
 package com.komponentes.oscomanager;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -9,17 +17,27 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class ListaPedidos extends Activity implements OnClickListener{
 
 	SharedPreferences preferences;
 	String URL, USER, PWD;
+	String TESTING = "{\"pedidos\":[{\"orders_id\":\"1\",\"customers_id\":\"2\",\"customers_name\":\"miguel reyes\",\"customers_company\":\"\",\"customers_street_address\":" +
+			"\"c/ hernan ruiz, 51\",\"customers_suburb\":\"\",\"customers_city\":null,\"customers_postcode\":\"41530\",\"customers_state\":\"Sevilla\",\"customers_country\"" +
+			":\"Spain\",\"customers_telephone\":\"652820639\",\"customers_email_address\":\"miguel.reyes@wanadoo.es\",\"customers_address_format_id\":\"3\",\"delivery_name\"" +
+			":\"miguel reyes\",\"delivery_company\":\"\",\"delivery_street_address\":\"c/ hernan ruiz, 51\",\"delivery_suburb\":\"\",\"delivery_city\"" +
+			":\"mor\u00f3n de la frontera\",\"delivery_postcode\":\"41530\",\"delivery_state\":\"Sevilla\",\"delivery_country\":\"Spain\",\"delivery_address_format_id\":\"3\"" +
+			",\"billing_name\":\"miguel reyes\",\"billing_company\":\"\",\"billing_street_address\":\"c/ hernan ruiz, 51\",\"billing_suburb\":\"\",\"billing_city\":null," +
+			"\"billing_postcode\":\"41530\",\"billing_state\":\"Sevilla\",\"billing_country\":\"Spain\",\"billing_address_format_id\":\"3\",\"payment_method\":\"PayPal " +
+			"Express\",\"cc_type\":\"\",\"cc_owner\":\"\",\"cc_number\":\"\",\"cc_expires\":\"\",\"last_modified\":\"2013-10-02 17:41:16\",\"date_purchased\":\"2013-09-23 " +
+			"18:03:45\",\"orders_status\":\"4\",\"orders_date_finished\":null,\"currency\":\"EUR\",\"currency_value\":\"1.000000\",\"order_products\":{\"orders_products_id\":\"1\",\"orders_id\":\"1\",\"products_id\":\"33\",\"products_model\":\"\",\"products_name\":\"Sodimm sdram pc133 512mb\",\"products_price\":\"20.0000\",\"final_price\":\"20.0000\",\"products_tax\":\"0.0000\",\"products_quantity\":\"1\"}}]}";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lista_pedidos);
-		
 		
 		// Preferencias
 		preferences = PreferenceManager.getDefaultSharedPreferences(this.getBaseContext());
@@ -32,6 +50,8 @@ public class ListaPedidos extends Activity implements OnClickListener{
 		button1.setOnClickListener(this);
 		View button2 = findViewById(R.id.button2);
 		button2.setOnClickListener(this);
+		
+		pintaLista(TESTING);
 	}
 	
 	@Override
@@ -84,5 +104,47 @@ public class ListaPedidos extends Activity implements OnClickListener{
 	    if (response != null){
 	    	Log.i("REST service response: ", response);
 	    }
+    }
+    
+    public void pintaLista(String response){
+		// ArrayList and ArrayAdapter
+		ListView listView = (ListView) findViewById(R.id.listView);
+		
+        // Create the grid item mapping
+		String[] from = new String[] {"fecha", "total"};
+		int[] to = new int[] {R.id.text1, R.id.text2};
+		
+		//HashMap<String, String> direcciones = new HashMap<String, String>();
+		List<String> fechas = new ArrayList<String>();
+		List<String> totales = new ArrayList<String>();
+		List<HashMap<String, String>> pedidos = new ArrayList<HashMap<String, String>>();
+
+		
+	    // Check if the connection fails
+	    if (response != null){
+			// Add messages from JSON response
+		    try {
+		    	JSONObject jsonMessages = new JSONObject(response);
+		    	JSONArray jsonArray = jsonMessages.getJSONArray("pedidos");
+		    	for (int i = 0; i < jsonArray.length(); i++) {
+			        JSONObject jsonObject = jsonArray.getJSONObject(i);
+			        fechas.add(jsonObject.getString("date_purchased"));
+			        totales.add(jsonObject.getJSONObject("order_products").getString("final_price"));
+		    	}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}	
+	    }
+
+		// Add each String,String element to a new HashMap with keys: "fechas", "totales"
+		for (int i=0; i<fechas.size(); i++) {
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put("fecha", fechas.get(i));
+			map.put("total", totales.get(i));
+			pedidos.add(map);
+		}
+
+		final SimpleAdapter adapter = new SimpleAdapter(this, pedidos, R.layout.simple_list_2lines, from, to);        
+		listView.setAdapter(adapter);
     }
 }
